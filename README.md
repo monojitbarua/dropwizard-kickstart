622a5e090cb519dec5285448b5a911e4a3dbf755
monojitbarua <monojit2012@gmail.com>
2020-05-31 14:40:39 +0530

    adding details in README.md

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Dropwizard sample project to kick start the journey.
+#Dropwizard sample project to kick start the journey.
 
 Drop wizard is a bunch of java framework glue together for the fast development of RESTful web services.
--- src/main/java/com/dropwizard/kickstart/healthcheck/EmployeeHealthCheck.java
+++ src/main/java/com/dropwizard/kickstart/healthcheck/EmployeeHealthCheck.java
@@ -3,6 +3,15 @@ package com.dropwizard.kickstart.healthcheck;
 import com.codahale.metrics.health.HealthCheck;
 import com.dropwizard.kickstart.dao.EmployeeDao;
 
+/**
+ * A health check is basically a small self-test which your application performs to verify that a specific component or responsibility is performing
+ * correctly. You can write your own custom logic there to verify specific module’s health.
+ * Dropwizard has a HTTP resource endpoint on /healthcheck on the admin port (default 8081)
+ *
+ * DropWizard health check is implemented by extending the HealthCheck class and
+ * returning Result.healthy() if everything is alright and Result.unhealthy() if something is not working – as expected.
+ * */
+
 public class EmployeeHealthCheck extends HealthCheck {
     @Override
     protected Result check() throws Exception {
